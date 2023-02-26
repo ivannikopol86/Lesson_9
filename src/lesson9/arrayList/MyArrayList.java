ae338ed885061c2bf22c0317a345296e5eadd8b1
Ivan Petrov <ivannikopol86@gmail.com>
2023-02-26 16:52:26 +0300

    DZ

---
+++ src/lesson9/arrayList/MyArrayList.java
@@ -0,0 +1,80 @@
+package lesson9.arrayList;
+
+
+import java.util.Arrays;
+import java.util.StringJoiner;
+
+//    https://github.com/stepangfil/GOIT/tree/main/src/main/java/ua/goit/module9
+
+public class MyArrayList<T> {
+    private static final int INIT_SIZE = 5;
+
+    private Object[] arr;
+    private int indexCounter;
+
+    public MyArrayList() {
+
+        arr = new Object[INIT_SIZE];
+    }
+
+    public void add(T value) {
+        if (indexCounter == arr.length - 1) {
+            resizeArr();
+        }
+        arr[indexCounter] = value;
+        indexCounter++;
+    }
+
+    public void remove(int index) {
+        arr[index] = null;
+        Object newArr[] = new Object[arr.length - 1];
+
+        clearElem(index);
+        createResizeArr();
+    }
+
+    public void clear() {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = null;
+        }
+    }
+
+    public int size() {
+        int counter = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public Object get(int index) {
+
+        return arr[index];
+    }
+
+    private void resizeArr() {
+
+        arr = Arrays.copyOf(arr, arr.length * 2);
+    }
+
+    private Object clearElem(int index) {
+        arr[index] = null;
+        for (; index < arr.length - 1; index++) {
+            arr[index] = arr[index + 1];
+        }
+        return arr;
+    }
+
+    private Object[] createResizeArr() {
+        Object[] newArr = new Object[arr.length - 1];
+        for (int i = 0; i < newArr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        return arr = newArr.clone();
+    }
+
+}
+
